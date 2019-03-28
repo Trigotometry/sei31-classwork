8028c534c6d78f3fd149595069c998c64d2aaba8
Thomas Hexton <tom.hexton@gmail.com>
2019-03-28 15:51:20 +1100

    syncing for home

---
+++ week-02/day-04/youtube-jQuery/js/main.js
@@ -0,0 +1,58 @@
+// const videos = document.querySelectorAll( 'li a' );
+//
+// // define empty array for URLs
+// const videoURLs = [];
+// const thumbNailURLs = [];
+//
+// for ( let i = 0; i < videos.length; i += 1 ) {
+// 	// push a tag URLs to array
+// 	videoURLs.push( videos[ i ].getAttribute( 'href' ) );
+// 	thumbNailURLs.push( youtube.generateThumbnailUrl( videoURLs[ i ] ) );
+//
+// 	image = document.createElement( 'img' );
+// 	image.setAttribute( 'src', thumbNailURLs[ i ] );
+//
+// 	videos[ i ].appendChild( image )
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+// joels v2
+
+// const links = document.querySelectorAll( 'li a' );
+//
+// for ( let i = 0; i < links.length; i += 1 ) {
+// 	const videoURLsV2 = links[ i ].getAttribute( 'href' );
+// 	const thumbnailURLsV2 = youtube.generateThumbnailUrl( videoURLsV2 );
+//
+// 	const thumbnail = document.createElement( 'img' );
+// 	thumbnail.setAttribute( 'src', thumbnailURLsV2 );
+//
+// 	links[ i ].appendChild( thumbnail )
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+// tom jQuery
+
+const $links = $( 'li a' );
+
+const thumbnailify = function ( $a ) {
+	const url = $a.attr( 'href' );
+	const thumbnailURL = youtube.generateThumbnailUrl( url );
+	const $thumbnail = $( '<img>' ).attr( 'src', thumbnailURL );
+	$a.append( $thumbnail );
+
+	$thumbnail.on('click', function (event) {
+		preventDefault();
+	});
+};
+
+for ( let i = 0; i < $links.length; i += 1 ) {
+	const $link = $( $links[ i ] ); // turns the vanilla DOM node backinto a jQuery node.
+	thumbnailify( $link );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+const divA = document.createElement('div');
+
+divA.innerHTML('hi.');
