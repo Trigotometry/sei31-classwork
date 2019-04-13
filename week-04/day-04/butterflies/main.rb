66f556b52963520be774263ac01b14a3a35817ae
Thomas Hexton <tom.hexton@gmail.com>
2019-04-13 22:55:30 +1000

    progress commit

--- week-04/day-04/butterflies/main.rb
+++ week-04/day-04/butterflies/main.rb
@@ -2,14 +2,10 @@ require "sinatra"
 require "sinatra/reloader"
 require "sqlite3"
 
-get "/" do
-	erb :home
-end
-
 # INDEX - show all butterflies
-get "/butterflies" do
+get "/" do
 	@butterflies = query_db "SELECT * FROM butterflies"
-	erb :butterflies_index
+	erb :index
 end
 
 # NEW - form for adding a new butterfly
@@ -18,20 +14,20 @@ get "/butterflies/new" do
 end
 
 # CREATE - add a new butterfly to the dartabase
-post '/butterflies' do
+post '/' do
 	query = "INSERT INTO butterflies (name, family, image) VALUES('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
 	query_db query
-	redirect to ('/butterflies') # GET request
+	redirect to ('/') # GET request
 end
 
-# SHOW - shows a single butterfly in more detail
+# SHOW
 get "/butterflies/:id" do
 	butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
 	@butterfly = butterflies.first
 	erb :butterflies_show
 end
 
-# EDIT - show a form to edit a single butterfly
+# EDIT
 get "/butterflies/:id/edit" do
 	butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
 	@butterfly = butterflies.first
@@ -40,15 +36,14 @@ end
 
 # UPDATE - modify the database with new information for a particular butterfly
 post "/butterflies/:id" do
-	query = "UPDATE butterflies SET name='#{params[:name]}', family='#{params[:family]}', image='#{params[:image]}' WHERE id=#{params[:id]}"
-	query_db query
+	query_db "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
 	redirect to ("/butterflies/#{ params[:id] }")
 end
 
 # DESTROY - delete a given butterfly form the database
-get "/butterflies/:id/delete" do
-	query_db "DELETE FROM butterflies WHERE id=#{ params[:id] }"
-	redirect to("/butterflies")
+get "/butterflies/:test/delete" do
+	query_db "DELETE FROM butterflies WHERE id=#{ params[:test] }"
+	redirect to("/")
 end
 
 def query_db(sql_statement)
