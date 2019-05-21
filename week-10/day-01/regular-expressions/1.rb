80dd15cffebcaf7cefaedf8f0ccc7517ff1420f9
Thomas Hexton <tom.hexton@gmail.com>
2019-05-22 08:51:51 +1000

    progress update

---
+++ week-10/day-01/regular-expressions/1.rb
@@ -0,0 +1,8 @@
+# lr.rb - A Ruby line reader (equivalent to cat)
+# Useage: ruby lr.rb /etc/passwd
+
+# ARGF is an array of th elines of each file mentioned in ARGV
+
+ARGF.each do | line |
+	puts line if line =~ /fred/
+end
